4c4829d15a0501dd31a41c43dc6066c97635a140
jslar28 <jslar28@gmail.com>
2025-07-14 23:42:23 +0200

    Hello Window

---
+++ Learning-OpenGL-with-FSharp/Learning-OpenGL-with-FSharp/Program.fs
@@ -0,0 +1,27 @@
+﻿open System.Drawing
+open Silk.NET.OpenGL
+open Silk.NET.Windowing
+
+[<EntryPoint>]
+let main argv =
+    printfn "Starting..."
+
+    let mutable options =  WindowOptions.Default
+    options.Title <- "Learning OpenGL in F#"
+    
+    let window = Window.Create options       
+    let mutable gl: GL = null
+
+    // Initialize gl in Load event (which is called after the window is created)
+    window.add_Load (fun () ->
+        gl <- GL.GetApi(window)
+    )
+
+    window.add_Render (
+         fun dt ->
+            if not (isNull gl) then
+                gl.ClearColor Color.Firebrick
+                gl.Clear (uint32 GLEnum.ColorBufferBit))
+
+    window.Run ()
+    0
