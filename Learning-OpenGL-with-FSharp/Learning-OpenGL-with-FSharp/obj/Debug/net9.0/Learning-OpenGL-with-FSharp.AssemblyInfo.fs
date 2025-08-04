def40d7d863227279e7c2d5da0fe4f2fc70881f1
jslar28 <jslar28@gmail.com>
2025-08-04 16:24:58 +0200

    Test commit

--- Learning-OpenGL-with-FSharp/Learning-OpenGL-with-FSharp/obj/Debug/net9.0/Learning-OpenGL-with-FSharp.AssemblyInfo.fs
+++ Learning-OpenGL-with-FSharp/Learning-OpenGL-with-FSharp/obj/Debug/net9.0/Learning-OpenGL-with-FSharp.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Learning-OpenGL-with-FSharp")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8365d239587d1aed29ca45df23a7bbfcbccd5e45")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4829d15a0501dd31a41c43dc6066c97635a140")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Learning-OpenGL-with-FSharp")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Learning-OpenGL-with-FSharp")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
